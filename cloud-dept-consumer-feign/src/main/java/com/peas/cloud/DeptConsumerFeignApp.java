ab047ea778fb91b9943cd4cb32f9db3902099ca9
7287 <7287@jics.cn>
2020-03-23 18:31:02 +0800

    添加eureka

--- cloud-dept-consumer-feign/src/main/java/com/peas/cloud/DeptConsumerFeignApp.java
+++ cloud-dept-consumer-feign/src/main/java/com/peas/cloud/DeptConsumerFeignApp.java
@@ -1,14 +1,16 @@
 package com.peas.cloud;
 
+import com.peas.rule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients(basePackages= {"com.peas.cloud"})
-//@RibbonClient(name="CLOUD-DEPT",configuration= MySelfRule.class)
+@RibbonClient(name="CLOUD-DEPT",configuration= MySelfRule.class)
 public class DeptConsumerFeignApp {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumerFeignApp.class,args);
