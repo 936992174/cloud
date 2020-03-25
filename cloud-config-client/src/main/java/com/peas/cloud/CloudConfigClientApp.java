e3732d6903bf9a7957c1e6bb86f39e6b68b8fe51
7287 <7287@jics.cn>
2020-03-25 17:48:43 +0800

    添加返回封装

---
+++ cloud-config-client/src/main/java/com/peas/cloud/CloudConfigClientApp.java
@@ -0,0 +1,11 @@
+package com.peas.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigClientApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigClientApp.class,args);
+    }
+}
